61070760c84b49042b5b3e48a1ce92cc16efa106
Jay Lee <jaylee.possible@gmail.com>
2020-09-17 03:56:51 +0900

    Update indiexing view

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('brain_indexing', views.brain_indexing, name='brain_indexing')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import generic
-from .models import Category
+from django.views import generic, View
+from .models import Category, Card, CardDetail
+import random
 
 
 class IndexView(generic.ListView):
@@ -10,3 +11,19 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Category.objects.order_by('updated_at')
+
+
+def brain_indexing(request):
+    picked_category_name = pick_randomly(request.GET.getlist('categories'))
+    random_category = Category.objects.filter(
+        name=picked_category_name)
+    print(random_category)
+
+    card = pick_randomly(Card.objects.filter(category=random_category).count())
+
+    return render(request, 'brain_indexing.html', {'card': card})
+
+
+def pick_randomly(dict):
+    idx = random.randint(0, len(dict) - 1)
+    return dict[idx]
